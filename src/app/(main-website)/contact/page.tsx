60c8012d71f25516ab1fb0523a46bcba7aaae819
panutat.p <pp.panutat@gmail.com>
2024-08-12 22:29:47 +0700

    more pages, contact, product

---
+++ src/app/(main-website)/contact/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+
+export default function ContactPage() {
+  return (
+    <Container>
+      <Typography variant="h3">Contact</Typography>
+    </Container>
+  )
+}
---
+++ src/app/(main-website)/product/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+
+export default function ProductPage() {
+  return (
+    <Container>
+      <Typography variant="h3">Product</Typography>
+    </Container>
+  )
+}
