782f6508a1a46858ce78cf43d57222e22a4d9215
panutat.p <pp.panutat@gmail.com>
2024-08-15 22:46:31 +0700

    simple fetch

--- src/app/(main-website)/about/page.tsx
+++ src/app/(main-website)/about/page.tsx
@@ -10,12 +10,23 @@ export const metadata: Metadata = {
   keywords: ['Next.js', 'React', 'TypeScript'],
 }
 
-export default function AboutPage() {
+async function getData() {
+  const res = await fetch('https://api.codingthailand.com/api/version')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+export default async function AboutPage() {
+  const res = await getData()
+
   return (
     <>
       <Container>
         <Typography variant="h3">About</Typography>
         <Typography>This is the about page</Typography>
+        <Typography>Version: {res.data.version}</Typography>
         <div>
           <Button component={Link} href="/" variant="contained" color="primary" sx={{ marginTop: 2, marginBottom: 2 }}>
             Go to Home
