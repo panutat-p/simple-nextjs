0d65884f5b05240348efda461b98dd8d04450cfd
panutat.p <pp.panutat@gmail.com>
2024-08-18 22:51:01 +0700

    fetch, 3 ways

--- src/app/(main-website)/product/service.ts
+++ src/app/(main-website)/product/service.ts
@@ -1,5 +1,25 @@
 export async function getProduct() {
+  const res = await fetch('https://api.codingthailand.com/api/course', { cache: 'no-store' })
+
+  if (!res.ok) {
+    throw new Error('Failed to GET /api/course')
+  }
+
+  return res.json()
+}
+
+export async function getProductCache() {
   const res = await fetch('https://api.codingthailand.com/api/course', { next: { revalidate: 10 } })
+
+  if (!res.ok) {
+    throw new Error('Failed to GET /api/course')
+  }
+
+  return res.json()
+}
+
+export async function getProductStatic() {
+  const res = await fetch('https://api.codingthailand.com/api/course')
 
   if (!res.ok) {
     throw new Error('Failed to GET /api/course')
