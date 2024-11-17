e183e67f5e4d144df4571aec5ee854ff5932bc20
panutat.p <pp.panutat@gmail.com>
2024-11-17 18:10:24 +0700

    product grids

--- src/app/(main-website)/product/page.tsx
+++ src/app/(main-website)/product/page.tsx
@@ -1,5 +1,7 @@
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
 import { getProduct } from './service'
 
 export default async function ProductPage() {
@@ -10,6 +12,19 @@ export default async function ProductPage() {
     <Container>
       <Typography variant="h3">Product</Typography>
       <Typography variant="body1">{products}</Typography>
+      {res.data && (
+        <>
+          <Grid container marginTop={5} spacing={3}>
+            {res.data.map((item: any) => {
+              return (
+                <Grid lg={3} item key={item.id}>
+                  <Paper>📘 {item.title}</Paper>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </>
+      )}
     </Container>
   )
 }
