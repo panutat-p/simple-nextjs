ef3e13d81d50b7b2beacbe592a4ada8164343329
panutat.p <pp.panutat@gmail.com>
2024-11-17 22:57:08 +0700

    style

--- src/app/(main-website)/product/page.tsx
+++ src/app/(main-website)/product/page.tsx
@@ -1,10 +1,10 @@
+import Image from 'next/image'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
-import Image from 'next/image'
-import { getProduct } from './service'
 import Link from '@mui/material/Link'
+import { getProduct } from './service'
 
 export default async function ProductPage() {
   const res = await getProduct()
