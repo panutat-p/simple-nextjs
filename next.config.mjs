72b16ea273b47960921eb9f29d445224f9ce1665
panutat.p <pp.panutat@gmail.com>
2024-11-17 22:55:54 +0700

    display image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   crossOrigin: 'anonymous',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.codingthailand.com',
+      },
+    ],
+  },
 }
 
 export default nextConfig
--- src/app/(main-website)/product/page.tsx
+++ src/app/(main-website)/product/page.tsx
@@ -2,29 +2,51 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
+import Image from 'next/image'
 import { getProduct } from './service'
+import Link from '@mui/material/Link'
 
 export default async function ProductPage() {
   const res = await getProduct()
-  const products = JSON.stringify(res, null, 2)
 
   return (
-    <Container>
-      <Typography variant="h3">Product</Typography>
-      <Typography variant="body1">{products}</Typography>
-      {res.data && (
-        <>
-          <Grid container marginTop={5} spacing={3}>
-            {res.data.map((item: any) => {
-              return (
-                <Grid lg={3} item key={item.id}>
-                  <Paper>📘 {item.title}</Paper>
-                </Grid>
-              )
-            })}
-          </Grid>
-        </>
-      )}
-    </Container>
+    <>
+      <Container>
+        <Typography variant="h3">Product</Typography>
+        {res.data && (
+          <>
+            <Grid container marginTop={5} spacing={3}>
+              {res.data.map((item: any) => {
+                return (
+                  <Grid container spacing={2} lg={3} item key={item.id}>
+                    <Paper>
+                      <Image src={item.picture} alt={item.detail} priority width={250} height={175} />
+                      <Typography align="center">{item.title}</Typography>
+                    </Paper>
+                  </Grid>
+                )
+              })}
+            </Grid>
+          </>
+        )}
+      </Container>
+      <Container sx={{ mt: 15 }}>
+        {res.data && (
+          <ul>
+            {res.data.map((item: any) => (
+              <li key={item.id}>
+                <Typography variant="body2">{item.title}</Typography>
+                <Typography variant="body2">{item.detail}</Typography>
+                <Typography variant="body2">Date: {item.date}</Typography>
+                <Typography variant="body2">Views: {item.view}</Typography>
+                <Link href={item.picture} target="_blank" rel="noopener noreferrer" underline="hover">
+                  {item.picture}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </Container>
+    </>
   )
 }
